6b7d61d899a04c6b5fa7f7034b3b1b7e5ebc0673
Consalvo <gonzaloceciliano@gmail.com>
2017-05-17 01:48:38 -0500

    First Push, Matrix Detectors

---
+++ Assets/Scripts/Instantiation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiation : MonoBehaviour {
+	public GameObject MatrixDetector;
+	void Start () {
+    for (int y = 0; y < 8; y++) {
+      for (int x = 0; x < 8; x++) {
+        var currentClone = Instantiate(MatrixDetector, new Vector3(-10.5f + x * 3, -0.35f, -10.5f + y * 3), Quaternion.identity);
+				currentClone.GetComponent<MetaData>().row = y;
+				currentClone.GetComponent<MetaData>().column = x;
+    }}
+	}
+}
---
+++ Assets/Scripts/MetaData.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetaData : MonoBehaviour {
+	public int row;
+	public int column;
+}
